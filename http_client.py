07dbffe2fb05317fcbba450e296efa35ab2472e1
Pankaj <pankaj@hotmail.com>
2026-08-17 18:42:01 +0530

    Added exception to process

--- http_client.py
+++ http_client.py
@@ -12,45 +12,54 @@ class HttpClient:
         """
         Perform a GET request with the specified headers.
         """
-        headers = {"accept": accept, "X-API-KEY": self.api_key}
-        response = requests.get(url, headers=headers, stream=True)
-        self.logger.info(f"GET URL - Status Code: {response.status_code}")
+        try:
+            headers = {"accept": accept, "X-API-KEY": self.api_key}
+            response = requests.get(url, headers=headers, stream=True)
+            self.logger.info(f"GET URL - Status Code: {response.status_code}")
 
-        self._handle_response(response)
+            self._handle_response(response)
 
-        content_type = response.headers.get("Content-Type", "")
-        self.logger.info(f"Content-Type: {content_type}")
-        if content_type in [
-            "application/zip",
-            "application/octect-stream",
-        ] and file_path:
-            # Save the ZIP file to the specified path
-            with open(file_path, "wb") as file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    if chunk:
-                        file.write(chunk)
-            return f"File saved to {file_path}"
-        elif accept == "application/json":
-            return response.json()
-        else:
-            return response.content
+            content_type = response.headers.get("Content-Type", "")
+            self.logger.info(f"Content-Type: {content_type}")
+            if content_type in [
+                "application/zip",
+                "application/octect-stream",
+            ] and file_path:
+                # Save the ZIP file to the specified path
+                with open(file_path, "wb") as file:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        if chunk:
+                            file.write(chunk)
+                return f"File saved to {file_path}"
+            elif accept == "application/json":
+                return response.json()
+            else:
+                return response.content
+        except requests.RequestException as e:
+            self.logger.error(f"GET request failed: {e}")
+            raise
 
     def post(self, url, accept="application/json",file_name=None):
         """
         Perform a POST request with the specified headers and optional file.
         """
-        headers = {"accept": accept, "X-API-KEY": self.api_key}
+        try:
+            headers = {"accept": accept, "X-API-KEY": self.api_key}
 
-        if file_name:
-            with open(file_name, "rb") as file:
-                files = {"files": file} if file else None
-                response = requests.post(url, headers=headers, files=files)
+            if file_name:
+                with open(file_name, "rb") as file:
+                    files = {"files": file} if file else None
+                    response = requests.post(url, headers=headers, files=files)
+                    self._handle_response(response)
+                    return response.json() if accept == "application/json" else response.content
+            else:
+                response = requests.post(url, headers=headers)
                 self._handle_response(response)
                 return response.json() if accept == "application/json" else response.content
-        else:
-            response = requests.post(url, headers=headers)
-            self._handle_response(response)
-            return response.json() if accept == "application/json" else response.content
+            
+        except requests.RequestException as e:
+            self.logger.error(f"POST request failed: {e}")
+            raise
 
     def _handle_response(self, response):
         """
